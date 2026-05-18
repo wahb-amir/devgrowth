d8011d33d0788bb22b8a784001b8fd5b0ac9e64e
wahb-amir <op422010@gmail.com>
2026-05-19 00:55:54 +0500

    Feat: added getPortfolio in SDK

--- backend/packages/discovery-sdk/src/client/portfolio.client.ts
+++ backend/packages/discovery-sdk/src/client/portfolio.client.ts
@@ -5,5 +5,9 @@ import { config } from "../config"
 const BASE = config.portfolio.baseUrl;
 
 
-export const githubClient = {
+export const portfolioClient = {
+    getPortfolio: (hostname:string) => {
+        const safeHostname = validateAndCleanGitHubUsername(hostname);
+        return http(`${BASE}/portfolio/${safeHostname}`);
+    }
 };
